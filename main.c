f754bac55ef986cc28169af65799009c0d0ac8d9
Breno-Lira <bmrl@cesar.school>
2024-10-26 18:54:24 -0300

    Implementando elementos graficos
    Adicinado background Reginaldo Rossi e um inimigo

--- main.c
+++ main.c
@@ -7,33 +7,58 @@
 #include "include/raygui.h"
 
 
-int main(void)
-{
-    // Inicializa a janela
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Minha Primeira Tela com Raylib");
 
-    // Define a taxa de frames por segundo
+int main(void){   
+    const int screenWidth = 776;
+    const int screenHeight = 522;
+    InitWindow(screenWidth, screenHeight, "Minha Primeira Tela com Raylib");
+    Texture praia, inimigo, torre;
+    praia = LoadTexture("./textures/Praia.png");
+    inimigo = LoadTexture("./textures/inimigo.png");
+    torre = LoadTexture("./textures/Torre-Reginaldo.png");
     SetTargetFPS(60);
 
-    // Loop principal do jogo
-    while (!WindowShouldClose()) // Verifica se a janela deve fechar
-    {
-        // Começa o desenho
+    float posX = 600;
+    int vida = 500;
+    int vidaMax = 500; // Vida máxima do inimigo
+    int larguraBarra = 50; // Largura máxima da barra de vida
+
+
+    while (!WindowShouldClose()) {
+        if (posX >= 100){
+            posX -= 0.8;
+        }
+       
         BeginDrawing();
 
-        // Limpa a tela com uma cor de fundo
         ClearBackground(RAYWHITE);
-
-        // Exibe uma mensagem no centro da tela
-        DrawText("Ola, Mundo!", 190, 200, 20, LIGHTGRAY);
-
-        // Finaliza o desenho
+        DrawTexture(praia, 0, 0, WHITE);
+        DrawTexture(torre, 20, 480-torre.height, WHITE);
+        
+         if (vida > 0){
+           DrawTexture(inimigo, posX, 500 - inimigo.height, WHITE);
+           vida--;
+           int larguraAtual = (vida * larguraBarra) / vidaMax;
+           
+           if (vida>340){
+            DrawRectangle(posX+40 , 500 - inimigo.height - 10, larguraAtual, 5, GREEN); 
+           }
+           else if (vida>150){
+             DrawRectangle(posX+40 , 500 - inimigo.height - 10, larguraAtual, 5, ORANGE); 
+           }
+           else{
+            DrawRectangle(posX+40 , 500 - inimigo.height - 10, larguraAtual, 5, RED); 
+           }
+           DrawRectangleLines(posX+40 , 500 - inimigo.height - 10, larguraBarra, 5, BLACK); 
+        }
+        else{
+            vida = 500;
+            posX = 600;
+        }
+        
         EndDrawing();
     }
 
-    // Fecha a janela e limpa os recursos
     CloseWindow();
 
     return 0;
