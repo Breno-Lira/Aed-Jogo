7e941528a061e60e8b1095dde1bc6c6e721cbf49
Breno-Lira <bmrl@cesar.school>
2024-10-27 18:03:39 -0300

    Consertando ataque Reginaldo
    Ajuste na lógica dos projeteis

--- main.c
+++ main.c
@@ -14,30 +14,32 @@ typedef struct Inimigo{
     int dano;
     double posX;
     double velocidade;
+    bool vivo;
 }Inimigo;
 
-void InitInimigo(Inimigo *inimigo, const char* foto, double vida,int xp,int dano, double posX, double velocidade);
+void InitInimigo(Inimigo *inimigo, const char* foto, double vida,int xp,int dano, double posX, double velocidade, bool vivo);
 void DrawInimigo(Inimigo *inimigo, int larguraBarra);
+void DrawAtaqueReginaldo(Inimigo *inimigos, Texture ReginaldoAtaque, double *posxataque);
 
 int main(void){   
     const int screenWidth = 776;
     const int screenHeight = 522;
     InitWindow(screenWidth, screenHeight, "Minha Primeira Tela com Raylib");
 
-    Texture praia, torre, ReginaldoAtaque;
+    Texture praia, torre;
     praia = LoadTexture("./textures/Praia.png");
     torre = LoadTexture("./textures/Torre-Reginaldo.png");
-    ReginaldoAtaque = LoadTexture("./textures/Reginaldo-ataque.png");
 
     SetTargetFPS(60);
 
-    Inimigo inimigoTeste;
-    Inimigo inimigoTeste2;
-    InitInimigo(&inimigoTeste, "./textures/inimigo.png", 500, 100, 20, 700, 0.8);
-    InitInimigo(&inimigoTeste2, "./textures/inimigo.png", 500, 100, 20, 1200, 2);
+    Inimigo inimigos[2];
+    int numInimigos = 0;
+    InitInimigo(&inimigos[numInimigos++], "./textures/inimigo.png", 500, 100, 20, 700, 0.8, true);
+    InitInimigo(&inimigos[numInimigos++], "./textures/inimigo.png", 500, 100, 20, 1200, 2.5, true);
     
+    Texture ReginaldoAtaque = LoadTexture("./textures/Reginaldo-ataque.png");
+    double posxataque = 25;
 
-    float posxataque = 25;
     int largurabarra = 50;
 
     while (!WindowShouldClose()) {
@@ -47,20 +49,21 @@ int main(void){
         DrawTexture(praia, 0, 0, WHITE);
         DrawTexture(torre, 20, 480-torre.height, WHITE);
 
-        if (posxataque  < inimigoTeste.posX-20){
-            DrawTexture(ReginaldoAtaque, posxataque, 480-ReginaldoAtaque.height, WHITE);
-            posxataque += 3;
-        }
-        else{
-            posxataque = 25;
-            inimigoTeste.vida -= 100; 
+        DrawAtaqueReginaldo(inimigos , ReginaldoAtaque , &posxataque);
+        
+         for (int i = 0; i < numInimigos; i++) {
+            DrawInimigo(&inimigos[i], largurabarra);
         }
         
-        DrawInimigo(&inimigoTeste, largurabarra);
-        DrawInimigo(&inimigoTeste2, largurabarra);
         
-        
+
+
+
         EndDrawing();
+    }
+
+    for (int i = 0; i < numInimigos; i++) {
+        UnloadTexture(inimigos[i].foto); 
     }
 
     CloseWindow();
@@ -69,13 +72,14 @@ int main(void){
 }
 
 
-void InitInimigo(Inimigo *inimigo, const char* foto, double vida,int xp,int dano, double posX, double velocidade) {
+void InitInimigo(Inimigo *inimigo, const char* foto, double vida,int xp,int dano, double posX, double velocidade, bool vivo) {
     inimigo->foto = LoadTexture(foto);
     inimigo->vida = vida;
     inimigo->vidaMax = vida;
     inimigo->dano = dano;
     inimigo->posX = posX;
     inimigo->velocidade = velocidade;
+    inimigo->vivo = vivo;
 }
 
 void DrawInimigo(Inimigo *inimigo, int larguraBarra){ 
@@ -84,7 +88,6 @@ void DrawInimigo(Inimigo *inimigo, int larguraBarra){
         }
 
     if (inimigo->vida > 0) {
-
         DrawTexture(inimigo->foto, inimigo->posX, 500 - inimigo->foto.height, WHITE);
         
         int larguraAtual = (inimigo->vida * larguraBarra) / inimigo->vidaMax;
@@ -100,9 +103,29 @@ void DrawInimigo(Inimigo *inimigo, int larguraBarra){
         DrawRectangleLines(inimigo->posX + 40, 500 - inimigo->foto.height - 10, larguraBarra, 5, BLACK);
     } 
     else {
-        //inimigo->vida = inimigo->vidaMax;
-        inimigo->posX = 1800;
+        inimigo->vivo = false;
+        inimigo->posX = 20000;
         UnloadTexture(inimigo->foto);
     }
 
+}
+
+
+void DrawAtaqueReginaldo(Inimigo *inimigos, Texture ReginaldoAtaque, double *posxataque) {
+    bool vivos = false;
+    for (int i = 0; i < 2; i++) {
+        if (inimigos[i].vivo == true){
+            vivos = true;
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        if (*posxataque < inimigos[i].posX - 20 && vivos) {
+            DrawTexture(ReginaldoAtaque, *posxataque, 480 - ReginaldoAtaque.height, WHITE);
+            *posxataque += 3;
+        } else {
+            *posxataque = 25;
+            inimigos[i].vida -= 100;
+        }
+    }
 }
